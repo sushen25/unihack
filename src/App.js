be8e0dc111229f6439fab23970b47a1eabe67557
Sushen Satturu <sushen.satturu@wspeonline.com>
2021-03-05 22:22:34 +1100

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Overview from './overview/Overview'
+import PotentialInvestments from './areas_of_improvement/potential_gains'
+import GoalSetting from "./goal_setting/goal_setting";
+
+import {Route, BrowserRouter as Router} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Route path="/" component={Overview} exact/>
+        <Route path="/improvement" component={PotentialInvestments} exact/>
+        <Route path="/goals" component={GoalSetting} exact/>
+      </Router>
     </div>
+    
   );
 }
 
---
+++ src/overview/Overview.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Overview extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>OVERVIEW</p>
+            </div>
+        );
+    }
+}
+
+export default Overview
