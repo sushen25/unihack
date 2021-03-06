02d5dee0e39bf015f753b156c9ac254048ea5332
John Kim <john.kim@monash.edu>
2021-03-06 18:05:19 +1100

    added monthly overview graph

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import Overview from './overview/Overview'
-import PotentialInvestments from './areas_of_improvement/potential_gains'
+import "./App.css";
+import Overview from "./overview/Overview";
+import PotentialInvestments from "./areas_of_improvement/potential_gains";
 import GoalSetting from "./goal_setting/goal_setting";
+import MonthlyOverview from "./overview/MonthlyOverview";
 
-import {Route, BrowserRouter as Router} from "react-router-dom"
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
       <Router>
-        <Route path="/" component={Overview} exact/>
-        <Route path="/improvement" component={PotentialInvestments} exact/>
-        <Route path="/goals" component={GoalSetting} exact/>
+        <Route path="/" component={Overview} exact />
+        <Route path="/improvement" component={PotentialInvestments} exact />
+        <Route path="/goals" component={GoalSetting} exact />
+        <Route path="/monthlyoverview" component={MonthlyOverview} exact />
       </Router>
     </div>
-    
   );
 }
 
